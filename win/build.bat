3d1b5fac93234f45d1be252e494d9004d9899fca
littlemole <littlemole@oha7.org>
2023-09-27 02:31:48 +0200

    fix build.bat

--- win/build.bat
+++ win/build.bat
@@ -5,7 +5,6 @@ cmake --build .
 cd ..
 
 cd x64-Release
-#cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE=%~dp0\..\vcpkg\scripts\buildsystems\vcpkg.cmake ../../..
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%~dp0\..\vcpkg\scripts\buildsystems\vcpkg.cmake ../../..
 cmake --build .
 
