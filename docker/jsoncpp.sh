86507c25edbb6f5aa15014e2cc8ddffe8fd7a97e
mike <mike@oha7.org>
2023-09-29 18:42:29 +0200

    jsoncpp static only take #3 sigh

--- docker/jsoncpp.sh
+++ docker/jsoncpp.sh
@@ -19,5 +19,5 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_CXX_FLAGS=$FLAGS  –DCMAKE_CXX_STANDARD=20 -DJSONCPP_LIB_BUILD_STATIC=ON -DBUILD_STATIC_LIBS=On -DBUILD_SHARED_LIBS=Off -DJSONCPP_WITH_TESTS=Off ..
 make
 make install
-rm /usr/local/lib/libjsoncpp.so*
+#rm /usr/local/lib/libjsoncpp.so*
   
